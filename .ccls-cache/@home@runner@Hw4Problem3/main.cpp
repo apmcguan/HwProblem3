7fbbe4ab7da60a9485e6d56c4d6e9b691b594303
apmcguan <111770168+apmcguan@users.noreply.github.com>
2022-09-19 04:56:32 +0000

    for loops

--- .ccls-cache/@home@runner@Hw4Problem3/main.cpp
+++ .ccls-cache/@home@runner@Hw4Problem3/main.cpp
@@ -5,7 +5,17 @@ using namespace std;
 
 int main() {
   int N, cases;
+  
   cin>> cases;
+  for (int low = 0; low < cases; low++)
+    {
+      cin >> N;
+      int fact = 1;
+      for (int i = 1; i <=N; i++)
+        {
+          
+        }
+    }
 
 
   
